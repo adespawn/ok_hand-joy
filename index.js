7841cb12c90a71407fc2408ab85625f1c51eb628
adespawn <adespawn@gmail.com>
2020-08-03 23:18:54 +0200

    synchroniczna obsługa :ok_hand::joy:

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const auth = require('./auth.json');
 var history = new Map();
 var interval = 5000;
+var tasks = [];
 client.login(auth.discord);
 client.on('ready', () => {
     fs.readFile('settings.set', 'utf8', function (err, data) {
@@ -18,45 +19,25 @@ client.on('ready', () => {
         console.log('✅ wczytano ustawienia bota');
     });
 });
-client.on('message', msg => {
-    if (msg.author.bot) return;
-    /*Premisje*/
-    //console.log(msg.member.roles.cache.some(role=>(role.permissions>>>3)%2===1));
-    /*koniec*/
-    if (msg.guild == null) {
-        return;
+var has_f = false;
+function handler() {
+    if (has_f == true) return;
+    has_f = true;
+    var msg = new Discord.Message();
+    while (tasks.length) {
+        msg = tasks.shift();
+        main(msg);
     }
-
-    if (history[msg.guild.id] == null) history[msg.guild.id] = new Map();
-    if (history[msg.guild.id][msg.author.id] == null) history[msg.guild.id][msg.author.id] = new Map();
-    if (history[msg.guild.id][msg.author.id]['correct'] == null) history[msg.guild.id][msg.author.id]['correct'] = 0;
-    if (history[msg.guild.id][msg.author.id]['wrong'] == null) history[msg.guild.id][msg.author.id]['wrong'] = 0;
-    if (history[msg.guild.id][msg.author.id]['speed'] == null) history[msg.guild.id][msg.author.id]['speed'] = 0;
-    if (history[msg.guild.id][msg.author.id]['last'] == null) history[msg.guild.id][msg.author.id]['last'] = 0;
-    if (msg.content == '!set here' && msg.guild.owner == msg.author) {
+    has_f = false;
+}
+function main(msg2) {
+    var msg = new Discord.Message();
+    msg = msg2;
+    if (history[msg.guild.id]['chanel'] != msg.channel.id
+        && msg.content == '!set here' && msg.guild.owner.id == msg.author.id) {
         history[msg.guild.id]['chanel'] = msg.channel.id;
         msg.channel.send("👌😂");
         return;
-    }
-    if (msg.content.startsWith('!') && history[msg.guild.id]['chanel'] != msg.channel.id) {
-        var args = msg.content.substring(1).split(' ');
-        var cmd = args[0].toLowerCase();
-        args = args.splice(1);
-        switch (cmd) {
-            case 'user':
-                let user=msg.mentions.members.first();
-                if(user==null){
-                    msg.channel.send('nie istnieje');
-                    break;
-                }
-                if(history[msg.guild.id][user.id]==null){
-                    msg.channel.send(user.username+' nic nie wysłał');
-                    break;
-                }
-                msg.channel.send(user.username+' wysłał '+history[msg.guild.id][user.id]['correct']
-                +' 👌😂 oraz '+(history[msg.guild.id][user.id]['wrong']+history[msg.guild.id][user.id]['speed'])+' niepoprawnych');
-                break;
-        }
     }
     if (history[msg.guild.id]['chanel'] == msg.channel.id) {
         if (Date.now() < history[msg.guild.id][msg.author.id]['last'] + interval) {
@@ -81,4 +62,50 @@ client.on('message', msg => {
     fs.writeFile('./settings.set', JSON.stringify(history), function (err) {
         if (err) return console.log(err);
     });
+}
+function settings(msg2) {
+    var msg = new Discord.Message();
+    msg = msg2;
+    if (history[msg.guild.id] == null) history[msg.guild.id] = new Map();
+    if (history[msg.guild.id][msg.author.id] == null) history[msg.guild.id][msg.author.id] = new Map();
+    if (history[msg.guild.id][msg.author.id]['correct'] == null) history[msg.guild.id][msg.author.id]['correct'] = 0;
+    if (history[msg.guild.id][msg.author.id]['wrong'] == null) history[msg.guild.id][msg.author.id]['wrong'] = 0;
+    if (history[msg.guild.id][msg.author.id]['speed'] == null) history[msg.guild.id][msg.author.id]['speed'] = 0;
+    if (history[msg.guild.id][msg.author.id]['last'] == null) history[msg.guild.id][msg.author.id]['last'] = 0;
+    history[msg.guild.id][msg.author.id]['nick'] = msg.author.username;
+    if (msg.content.startsWith('!') && history[msg.guild.id]['chanel'] != msg.channel.id) {
+        var args = msg.content.substring(1).split(' ');
+        var cmd = args[0].toLowerCase();
+        args = args.splice(1);
+        switch (cmd) {
+            case 'user':
+                let user = msg.mentions.members.first();
+                if (user == null) {
+                    msg.channel.send('nie istnieje');
+                    break;
+                }
+                if (history[msg.guild.id][user.id] == null) {
+                    msg.channel.send(user.username + ' nic nie wysłał');
+                    break;
+                }
+                msg.channel.send(history[msg.guild.id][user.id]['nick'] + ' wysłał ' + history[msg.guild.id][user.id]['correct']
+                    + ' 👌😂 oraz ' + (history[msg.guild.id][user.id]['wrong'] + history[msg.guild.id][user.id]['speed']) + ' niepoprawnych wiadomości');
+                break;
+            case 'rank':
+                break;
+        }
+    }
+}
+client.on('message', msg => {
+    if (msg.author.bot) return;
+    /*Premisje*/
+    //console.log(msg.member.roles.cache.some(role=>(role.permissions>>>3)%2===1));
+    /*koniec*/
+    if (msg.guild == null) {
+        return;
+    }
+    settings(msg);
+    tasks.push(msg);
+    handler();
+
 });
